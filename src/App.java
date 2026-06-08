e9b6bf579317d0074917fed364cea670d9caf1a2
Carlos Tello <ctellob3114@gmail.com>
2026-06-08 07:58:59 -0500

    First Commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        runLinkedList();
+    }
+
+    private static void runLinkedList() {
+        System.out.println("Lista Enlazada / LinkedList");
+        LinkedList<String> nombres = new LinkedList<>();
+    }
+}
